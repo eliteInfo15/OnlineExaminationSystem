c8c2ed3aba9bacb5e8ada3cf4fdf11e174e0a03b
Mansishukla08 <shuklamansi800@gmail.com>
2020-09-13 17:20:04 +0530

    done

--- OnlineExamApp/views.py
+++ OnlineExamApp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from .models import admin
-from .serializers import AdminSerializers
+from.models import Admin
+from.serializers import AdminSerializers
 
 class Admininfo(viewsets.ModelViewSet):
     queryset =Admin.objects.all()
