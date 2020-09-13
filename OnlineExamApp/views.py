c06025ef29908e4ad9a6b01b937b65abb090f43d
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-13 17:05:07 +0530

    Update views.py

--- OnlineExamApp/views.py
+++ OnlineExamApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import admin
+from .serializers import AdminSerializers
+
+class Admininfo(viewsets.ModelViewSet):
+    queryset =Admin.objects.all()
+    serializer_class=AdminSerializer
+    
