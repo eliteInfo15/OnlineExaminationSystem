28d6b3c08fc00851bad3b5e69dc7b6074743ecce
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-16 11:27:45 +0530

    Update serializers.py
    Serializers done

--- OnlineExamApp/serializers.py
+++ OnlineExamApp/serializers.py
@@ -5,3 +5,24 @@ class AdminSerializers(serializers.ModelSerializers):
     class Meta:
         model=admin
         fields=all
+
+class Categoryserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields="__all__"
+
+class Centerserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Center
+        fields="__all__"        
+
+class Batchserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Batch
+        fields="__all__"        
+        
+class Subcategoryserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Subcategory
+        fields="__all__"        
+        
