8219370fccc1d87547fbf5290ee25c218d866a68
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-16 11:30:06 +0530

    Update views.py

--- OnlineExamApp/views.py
+++ OnlineExamApp/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from.models import Admin
-from.serializers import AdminSerializers
+from.models import Admin,Category,Batch,Center,Subcategory
+from.serializers import AdminSerializers,Categoryserializer,Batchserializer,Centerserializer,Subcategoryserializer
 
 class Admininfo(viewsets.ModelViewSet):
     queryset =Admin.objects.all()
     serializer_class=AdminSerializer
+    
+
+class Categoryview(viewsets.ModelViewSet):
+    queryset=Category.objects.all()
+    serializer_class=Categoryserializer
+    
+class Centerview(viewsets.ModelViewSet):
+    queryset=Center.objects.all()
+    serializer_class=Centerserializer
+    
+class Batchview(viewsets.ModelViewSet):
+    queryset=Batch.objects.all()
+    serializer_class=Batchserializer        
+
+class Subcategoryview(viewsets.ModelViewSet):
+    queryset=Subcategory.objects.all()
+    serializer_class=Subcategoryserializer      
     
