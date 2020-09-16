d4c6e18239f0b1a082af234a56ea8d9038821533
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-16 11:31:47 +0530

    Create urls.py

---
+++ OnlineExamApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('category',views.Categoryview)
+router.register('batch',views.Batchview)
+router.register('center',views.Centerview)
+router.register('subcategory',views.Subcategoryview)
+urlpatterns = [
+    path('',include(router.urls))
+]
