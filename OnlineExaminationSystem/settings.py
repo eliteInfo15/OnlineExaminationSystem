5b672e4ad2bad68a7f9a6ebba3de2beb1dc449ca
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-29 21:33:24 +0530

    Update settings.py

--- OnlineExaminationSystem/settings.py
+++ OnlineExaminationSystem/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OnlineExamApp',
+    'rest_framework
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'OnlineExaminationSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'examdb',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'3306' }
     }
 }
 
