a687de7f77df2ef6f9041a7774fa7f34e8a3b0d9
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-29 17:36:37 +0530

    Update urls.py

--- OnlineExamApp/urls.py
+++ OnlineExamApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('admin',views.AdminInfo)
+router.register('admin',views.Admininfo)
 router.register('category',views.Categoryview)
 router.register('batch',views.Batchview)
 router.register('center',views.Centerview)
