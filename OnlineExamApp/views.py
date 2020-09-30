f1630e0693c04014c16f80c9ad672f753b09fd23
Mansishukla08 <60772133+Mansishukla08@users.noreply.github.com>
2020-09-30 11:35:14 +0530

    Update views.py

--- OnlineExamApp/views.py
+++ OnlineExamApp/views.py
@@ -1,28 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from.models import Admin
+from.serializers import Adminserializer
+class Adminview(viewsets.ModelViewSet):
+    queryset=Admin.objects.all()
+    serializer_class=Adminserializer
 
 # Create your views here.
-from rest_framework import viewsets
-from.models import Admin,Category,Batch,Center,Subcategory
-from.serializers import AdminSerializers,Categoryserializer,Batchserializer,Centerserializer,Subcategoryserializer
-
-class Admininfo(viewsets.ModelViewSet):
-    queryset =Admin.objects.all()
-    serializer_class=AdminSerializer
-    
-
-class Categoryview(viewsets.ModelViewSet):
-    queryset=Category.objects.all()
-    serializer_class=Categoryserializer
-    
-class Centerview(viewsets.ModelViewSet):
-    queryset=Center.objects.all()
-    serializer_class=Centerserializer
-    
-class Batchview(viewsets.ModelViewSet):
-    queryset=Batch.objects.all()
-    serializer_class=Batchserializer        
-
-class Subcategoryview(viewsets.ModelViewSet):
-    queryset=Subcategory.objects.all()
-    serializer_class=Subcategoryserializer      
-    
+class Adminbyname(APIView):
+    def get(self,request,name):
+        Admins=Admin.objects.get(username=name)
+        serializer=Admin
+        return Response(serializer.data)
+      
